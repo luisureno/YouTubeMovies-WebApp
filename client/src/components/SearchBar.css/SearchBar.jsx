19616e4fe62b57b1ebe51daadb60eb92a6dadc48
Luis Angel Ureno <lureno2@horizon.csueastbay.edu>
2025-02-26 12:10:14 -0800

    adding search functionalities

--- client/src/components/SearchBar.css/SearchBar.jsx
+++ client/src/components/SearchBar.css/SearchBar.jsx
@@ -1,9 +1,34 @@
 import './SearchBar.css';
+import React from 'react';
+import { useState } from 'react';
+import {movieData} from '../../movieData';
+import Main from '../Main/Main';
 
 export default function SearchBar(){
+    // this state holds the current value of the search input, and 
+    // is updated whenever the user types in the search bar
+    const [searchTerm, setSearchTerm] = useState('');
+    // this state holds the filtered movies that match the search term
+    // and is calculated directly from the movieData
+    const [filteredMovies, setFilteredMovies] = useState([]);
+
+    const handSeachChange = (e) => {
+        //this grabs the current text in the search bar
+        const value = e.target.value;
+        //this will update the search term state with the current value
+        //inside the input field
+        setSearchTerm(value);
+
+    };
+
+
+
     return (
         <form className="search-bar">
-            <input type="text" placeholder="Search for a movie..." />
+            <input 
+            type="text" 
+            placeholder="Search for a movie..." 
+            />
             <button className='searchbar-btn'>Search</button>
         </form>
     )
